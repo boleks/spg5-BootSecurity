14a6fdce46d407154ea540545d04145bf557d909
Boleks <btrosic@gmail.com>
2020-02-15 12:56:21 +0100

    02 - Form Authentication

--- src/main/java/com/boleks/spg5BootSecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/boleks/spg5BootSecurity/security/ApplicationSecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.boleks.spg5BootSecurity.security.ApplicationUserPermission.*;
 import static com.boleks.spg5BootSecurity.security.ApplicationUserRole.*;
@@ -43,7 +46,23 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/courses", true)
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("Ovdeunesikey")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("remember-me", "JSESSIONID")
+                    .logoutSuccessUrl("/login");
+
+
 
 
 //        http
